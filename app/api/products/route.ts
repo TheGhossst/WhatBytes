dbd8d3fdfd22c444500ec7784a7867d9c50afd4b
TheGhossst <nandukrishnapkm97@gmail.com>
2025-06-30 18:51:35 +0530

    feat: caching in api route

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,19 +1,22 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const response = await fetch('https://fakestoreapi.com/products');
+    const response = await fetch("https://fakestoreapi.com/products", {
+      next: { revalidate: 3600 },
+    });
     if (!response.ok) {
-      throw new Error('Failed to fetch products from Fake Store API');
+      throw new Error("Failed to fetch products from Fake Store API");
     }
     const data = await response.json();
     return NextResponse.json(data);
   } catch (error) {
     console.error(error);
-    const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
+    const errorMessage =
+      error instanceof Error ? error.message : "An unknown error occurred";
     return new NextResponse(
       JSON.stringify({ message: `Failed to fetch products: ${errorMessage}` }),
-      { status: 500, headers: { 'Content-Type': 'application/json' } }
+      { status: 500, headers: { "Content-Type": "application/json" } }
     );
   }
 }
